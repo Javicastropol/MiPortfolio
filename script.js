d6fe12b1b935ff2fc86b029bae616c04b854d8e9
Javicastropol <119880074+Javicastropol@users.noreply.github.com>
2023-05-15 13:40:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let menuVisible = false;
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  }
+  else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+
+
+function elementIsInView(element) {
+    var top = element.offsetTop;
+    var left = element.offsetLeft;
+    var width = element.offsetWidth;
+    var height = element.offsetHeight;
+
+    while (element.offsetParent) {
+        element = element.offsetParent;
+        top += element.offsetTop;
+        left += element.offsetLeft;
+    }
+
+    return (
+        top >= window.pageYOffset &&
+        left >= window.pageXOffset &&
+        top + height <= window.pageYOffset + window.innerHeight &&
+        left + width <= window.pageXOffset + window.innerWidth
+    );
+}
+
+window.addEventListener("scroll", function (e) {
+  let elements = document.querySelectorAll(".barra-skill");
+  elements.forEach((e) => { 
+    if (elementIsInView(e)) { 
+      let progreso_element = e.querySelector(".progreso");
+      let width = progreso_element.getAttribute('data-progress_percent')+'%';
+      progreso_element.style.width = width;
+    }
+  })
+}, { passive: true });
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_z4dqojz';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar Email';
+      alert('Enviado!');
+    }, (err) => {
+      btn.value = 'Enviar Email';
+      alert(JSON.stringify(err));
+    });
+});
